cf0691c096cbbf2171fdc0f301724b4aae3467ff
emrebezci <emrebezci@protonmail.com>
2021-10-15 12:01:14 +0300

    mobil nav eklendi

---
+++ js/main.js
@@ -0,0 +1,33 @@
+function hamburgeropen() {
+
+    var open = document.getElementById("mobile-nav-isactive");
+    if (open.style.display === "none") {
+        open.style.display = "block";
+    } else {
+        open.style.display = "none";
+    }
+    var close = document.getElementById("mobile-nav");
+    if (close.style.display === "block") {
+        close.style.display = "none";
+    } else {
+        close.style.display = "block";
+    }
+}
+
+
+
+function hamburgerClose() {
+
+    var open = document.getElementById("mobile-nav-isactive");
+    if (open.style.display === "none") {
+        open.style.display = "block";
+    } else {
+        open.style.display = "none";
+    }
+    var close = document.getElementById("mobile-nav");
+    if (close.style.display === "block") {
+        close.style.display = "none";
+    } else {
+        close.style.display = "block";
+    }
+}
